f6f34f708cc94d01c0b076f0560d792383af5c7b
ZhongShengping <chdzsp@163.com>
2016-10-27 22:47:46 +0800

    Add support for db_max_retries param
    The db_max_retries parameter regulates the number of reconnection
    attempts performed after an error raised rather than at startup.
    
    Change-Id: I21f332e4c8b1083524e3ff3eb13016f5e6c93516
    Closes-Bug: 1579718

--- spec/classes/panko_db_spec.rb
+++ spec/classes/panko_db_spec.rb
@@ -7,6 +7,7 @@ describe 'panko::db' do
       it { is_expected.to contain_panko_config('database/connection').with_value('sqlite:////var/lib/panko/panko.sqlite') }
       it { is_expected.to contain_panko_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_panko_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_panko_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_panko_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_panko_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_panko_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
@@ -18,6 +19,7 @@ describe 'panko::db' do
         { :database_connection     => 'mysql+pymysql://panko:panko@localhost/panko',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
+          :database_db_max_retries => '-1',
           :database_max_retries    => '11',
           :database_retry_interval => '11',
           :database_max_pool_size  => '11',
@@ -28,6 +30,7 @@ describe 'panko::db' do
       it { is_expected.to contain_panko_config('database/connection').with_value('mysql+pymysql://panko:panko@localhost/panko') }
       it { is_expected.to contain_panko_config('database/idle_timeout').with_value('3601') }
       it { is_expected.to contain_panko_config('database/min_pool_size').with_value('2') }
+      it { is_expected.to contain_panko_config('database/db_max_retries').with_value('-1') }
       it { is_expected.to contain_panko_config('database/max_retries').with_value('11') }
       it { is_expected.to contain_panko_config('database/retry_interval').with_value('11') }
       it { is_expected.to contain_panko_config('database/max_pool_size').with_value('11') }
