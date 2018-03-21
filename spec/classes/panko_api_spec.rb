cfd9d68decaf6bf078c17ccd09e2bbf071732efd
ZhongShengping <chdzsp@163.com>
2018-03-21 11:31:33 +0800

    oslo middleware: check puppet resource instead of actual config in spec
    Change-Id: I4bdb0249cb36a3497ecedf31bf05c748eda2d170

--- spec/classes/panko_api_spec.rb
+++ spec/classes/panko_api_spec.rb
@@ -48,7 +48,9 @@ describe 'panko::api' do
       is_expected.to contain_panko_config('storage/retry_interval').with_value(params[:retry_interval])
       is_expected.to contain_panko_config('storage/es_ssl_enabled').with_value(params[:es_ssl_enabled])
       is_expected.to contain_panko_config('storage/es_index_name').with_value(params[:es_index_name])
-      is_expected.to contain_panko_config('oslo_middleware/enable_proxy_headers_parsing').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_oslo__middleware('panko_config').with(
+        :enable_proxy_headers_parsing => '<SERVICE DEFAULT>',
+      )
       is_expected.to contain_panko_config('database/event_time_to_live').with_value( params[:event_time_to_live] )
     end
 
@@ -86,7 +88,9 @@ describe 'panko::api' do
         params.merge!({:enable_proxy_headers_parsing => true })
       end
 
-      it { is_expected.to contain_panko_config('oslo_middleware/enable_proxy_headers_parsing').with_value(true) }
+      it { is_expected.to contain_oslo__middleware('panko_config').with(
+        :enable_proxy_headers_parsing => true,
+      )}
     end
 
     context 'with disabled service managing' do
