f92f708d4c1c2f67ed0da709e0553341dda67432
ZhongShengping <chdzsp@163.com>
2018-03-29 12:01:14 +0800

    Remove deprecated ensure_package option
    Change-Id: I40cc47c2001e86914177cb38a91392fa8d9a56ba

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,32 +13,16 @@
 #   in the panko config.
 #   Defaults to false.
 #
-# DEPRECATED PARAMETERS
-#
-# [*ensure_package*]
-#   (optional) The state of panko packages
-#   Defaults to undef
-#
 class panko (
   $package_ensure      = 'present',
   $purge_config        = false,
-  # DEPRECATED PARAMETERS
-  $ensure_package      = undef,
 ) inherits panko::params {
 
   include ::panko::deps
   include ::panko::logging
 
-  if $ensure_package {
-    warning("panko::ensure_package is deprecated and will be removed in \
-the future release. Please use panko::package_ensure instead.")
-    $package_ensure_real = $ensure_package
-  } else {
-    $package_ensure_real = $package_ensure
-  }
-
   package { 'panko':
-    ensure => $package_ensure_real,
+    ensure => $package_ensure,
     name   => $::panko::params::common_package_name,
     tag    => ['openstack', 'panko-package'],
   }
