86a8ae99924c2ebd833a2a0e644aaf5d379d0868
huang.zhiping <huang.zhiping@99cloud.net>
2018-05-26 17:41:37 +0800

    Replace port 35357 with 5000 for "auth_url"
    Based on the change in Keystone Install Guide [1],
    this patch replace port 35357 with 5000 for "auth_url".
    
    For more details, please check similar changes which have been done
    on other projects: Nova [2], Neutron [3], Cinder [4], Glance [5].
    
    [1] https://review.openstack.org/#/c/541857
    [2] https://review.openstack.org/#/c/562812
    [3] https://review.openstack.org/#/c/566491
    [4] https://review.openstack.org/#/c/565464
    [5] https://review.openstack.org/#/c/558932
    
    Change-Id: Ic3d67f6ef1f7fe980154887b62e38155985573c8

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -13,7 +13,7 @@
 #
 # [*auth_url*]
 #   (Optional) The URL to use for authentication.
-#   Defaults to 'http://localhost:35357'.
+#   Defaults to 'http://localhost:5000'.
 #
 # [*project_name*]
 #   (Optional) Service project name
@@ -191,7 +191,7 @@
 class panko::keystone::authtoken(
   $password,
   $username                       = 'panko',
-  $auth_url                       = 'http://localhost:35357',
+  $auth_url                       = 'http://localhost:5000',
   $project_name                   = 'services',
   $user_domain_name               = 'Default',
   $project_domain_name            = 'Default',
