605d1e553842218c37c8164991fad2a9d287ee27
Tobias Urdin <tobias.urdin@binero.se>
2018-11-24 15:41:50 +0100

    Convert to rspec-puppet-facts
    Change-Id: I784e9b8f85ba04760265e819ad46c68381388fa8

--- spec/classes/panko_expirer_spec.rb
+++ spec/classes/panko_expirer_spec.rb
@@ -1,22 +1,16 @@
-#
-# Unit tests for panko::expirer
-#
-
 require 'spec_helper'
 
 describe 'panko::expirer' do
-
-  shared_examples_for 'panko-expirer' do
-
-    it { is_expected.to contain_class('panko::deps') }
-    it { is_expected.to contain_class('panko::params') }
-
+  shared_examples 'panko::expirer' do
     let :params do
-        { }
+      {}
     end
 
-    it 'configures a cron' do
-      is_expected.to contain_cron('panko-expirer').with(
+    context 'with default' do
+      it { is_expected.to contain_class('panko::deps') }
+      it { is_expected.to contain_class('panko::params') }
+
+      it { is_expected.to contain_cron('panko-expirer').with(
         :ensure      => 'present',
         :command     => 'panko-expirer',
         :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
@@ -26,28 +20,37 @@ describe 'panko::expirer' do
         :monthday    => '*',
         :month       => '*',
         :weekday     => '*'
-      )
+      )}
     end
 
     context 'with cron not enabled' do
       before do
-        params.merge!({
-          :enable_cron => false })
+        params.merge!( :enable_cron => false )
       end
-      it {
-        is_expected.to contain_cron('panko-expirer').with(
-          :ensure      => 'absent',
-          :command     => 'panko-expirer',
-          :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
-          :user        => 'panko',
-          :minute      => 1,
-          :hour        => 0,
-          :monthday    => '*',
-          :month       => '*',
-          :weekday     => '*'
-        )
-      }
-    end
 
+      it { is_expected.to contain_cron('panko-expirer').with(
+        :ensure      => 'absent',
+        :command     => 'panko-expirer',
+        :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
+        :user        => 'panko',
+        :minute      => 1,
+        :hour        => 0,
+        :monthday    => '*',
+        :month       => '*',
+        :weekday     => '*'
+      )}
+    end
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'panko::expirer'
+    end
   end
 end
