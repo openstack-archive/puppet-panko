ad0b8af0f40fc075cb57d4a0e87e46af27a4ba31
ZhongShengping <chdzsp@163.com>
2019-04-28 15:00:12 +0800

    Deprecate idle_timeout option
    The idle_timeout parameter is deprecated, use connection_recycle_time
    instead[1].
    
    [1]https://review.opendev.org/#/c/334182/
    
    Change-Id: I684b2cd4973adc7d4ece83e620fd053e191bb19f
    Depends-On: https://review.opendev.org/656106/
    Closes-Bug: #1826692

--- manifests/db.pp
+++ manifests/db.pp
@@ -8,7 +8,7 @@
 #   (Optional) Url used to connect to database.
 #   Defaults to 'sqlite:////var/lib/panko/panko.sqlite'.
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   (Optional) Timeout when db connections should be reaped.
 #   Defaults to $::os_service_default
 #
@@ -42,32 +42,46 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class panko::db (
-  $database_connection     = 'sqlite:////var/lib/panko/panko.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/panko/panko.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::panko::deps
+
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+  $database_connection_recycle_time_real = pick($database_idle_timeout, $database_connection_recycle_time)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql(\+psycopg2)?):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'panko_config':
-    connection     => $database_connection,
-    idle_timeout   => $database_idle_timeout,
-    min_pool_size  => $database_min_pool_size,
-    db_max_retries => $database_db_max_retries,
-    max_retries    => $database_max_retries,
-    retry_interval => $database_retry_interval,
-    max_pool_size  => $database_max_pool_size,
-    max_overflow   => $database_max_overflow,
-    pool_timeout   => $database_pool_timeout,
+    connection              => $database_connection,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size,
+    db_max_retries          => $database_db_max_retries,
+    max_retries             => $database_max_retries,
+    retry_interval          => $database_retry_interval,
+    max_pool_size           => $database_max_pool_size,
+    max_overflow            => $database_max_overflow,
+    pool_timeout            => $database_pool_timeout,
   }
 }
--- spec/classes/panko_db_spec.rb
+++ spec/classes/panko_db_spec.rb
@@ -6,44 +6,44 @@ describe 'panko::db' do
       it { should contain_class('panko::deps') }
 
       it { should contain_oslo__db('panko_config').with(
-        :db_max_retries => '<SERVICE DEFAULT>',
-        :connection     => 'sqlite:////var/lib/panko/panko.sqlite',
-        :idle_timeout   => '<SERVICE DEFAULT>',
-        :min_pool_size  => '<SERVICE DEFAULT>',
-        :max_pool_size  => '<SERVICE DEFAULT>',
-        :max_retries    => '<SERVICE DEFAULT>',
-        :retry_interval => '<SERVICE DEFAULT>',
-        :max_overflow   => '<SERVICE DEFAULT>',
-        :pool_timeout   => '<SERVICE DEFAULT>',
+        :db_max_retries          => '<SERVICE DEFAULT>',
+        :connection              => 'sqlite:////var/lib/panko/panko.sqlite',
+        :connection_recycle_time => '<SERVICE DEFAULT>',
+        :min_pool_size           => '<SERVICE DEFAULT>',
+        :max_pool_size           => '<SERVICE DEFAULT>',
+        :max_retries             => '<SERVICE DEFAULT>',
+        :retry_interval          => '<SERVICE DEFAULT>',
+        :max_overflow            => '<SERVICE DEFAULT>',
+        :pool_timeout            => '<SERVICE DEFAULT>',
       )}
     end
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://panko:panko@localhost/panko',
-          :database_idle_timeout   => '3601',
-          :database_min_pool_size  => '2',
-          :database_db_max_retries => '-1',
-          :database_max_retries    => '11',
-          :database_retry_interval => '11',
-          :database_max_pool_size  => '11',
-          :database_max_overflow   => '21',
-          :database_pool_timeout   => '21',
+        { :database_connection              => 'mysql+pymysql://panko:panko@localhost/panko',
+          :database_connection_recycle_time => '3601',
+          :database_min_pool_size           => '2',
+          :database_db_max_retries          => '-1',
+          :database_max_retries             => '11',
+          :database_retry_interval          => '11',
+          :database_max_pool_size           => '11',
+          :database_max_overflow            => '21',
+          :database_pool_timeout            => '21',
         }
       end
 
       it { should contain_class('panko::deps') }
 
       it { should contain_oslo__db('panko_config').with(
-        :db_max_retries => '-1',
-        :connection     => 'mysql+pymysql://panko:panko@localhost/panko',
-        :idle_timeout   => '3601',
-        :min_pool_size  => '2',
-        :max_pool_size  => '11',
-        :max_retries    => '11',
-        :retry_interval => '11',
-        :max_overflow   => '21',
-        :pool_timeout   => '21',
+        :db_max_retries          => '-1',
+        :connection              => 'mysql+pymysql://panko:panko@localhost/panko',
+        :connection_recycle_time => '3601',
+        :min_pool_size           => '2',
+        :max_pool_size           => '11',
+        :max_retries             => '11',
+        :retry_interval          => '11',
+        :max_overflow            => '21',
+        :pool_timeout            => '21',
       )}
     end
   end
