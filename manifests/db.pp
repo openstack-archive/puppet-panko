c7ce5143b98cb1c69489d0f9370e60acab618d7a
ZhongShengping <chdzsp@163.com>
2020-03-23 14:45:49 +0800

    Remove idle_timeout option
    The idle_timeout parameter has been deprecated for two releases.
    We can remove it.
    
    Change-Id: I3bfd37d17fd090847612f8a45b2787b1615d8e24

--- manifests/db.pp
+++ manifests/db.pp
@@ -42,12 +42,6 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
-# DEPRECATED PARAMETERS
-#
-# [*database_idle_timeout*]
-#   Timeout when db connections should be reaped.
-#   Defaults to undef.
-#
 class panko::db (
   $database_connection              = 'sqlite:////var/lib/panko/panko.sqlite',
   $database_connection_recycle_time = $::os_service_default,
@@ -58,24 +52,16 @@ class panko::db (
   $database_retry_interval          = $::os_service_default,
   $database_max_overflow            = $::os_service_default,
   $database_pool_timeout            = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $database_idle_timeout            = undef,
 ) {
 
   include panko::deps
-
-  if $database_idle_timeout {
-    warning('The database_idle_timeout parameter is deprecated. Please use \
-database_connection_recycle_time instead.')
-  }
-  $database_connection_recycle_time_real = pick($database_idle_timeout, $database_connection_recycle_time)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql(\+psycopg2)?):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'panko_config':
     connection              => $database_connection,
-    connection_recycle_time => $database_connection_recycle_time_real,
+    connection_recycle_time => $database_connection_recycle_time,
     min_pool_size           => $database_min_pool_size,
     db_max_retries          => $database_db_max_retries,
     max_retries             => $database_max_retries,
