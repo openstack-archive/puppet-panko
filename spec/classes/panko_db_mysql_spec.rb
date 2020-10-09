89c2b417169863a191249a5ab56c38ae4ce893bd
ZhongShengping <chdzsp@163.com>
2020-10-09 09:59:55 +0800

    Include deps class in unit test for mysql
    Change-Id: I3bd31e64eac9ee98bbf7863b52107e6942f486dc

--- spec/classes/panko_db_mysql_spec.rb
+++ spec/classes/panko_db_mysql_spec.rb
@@ -16,6 +16,8 @@ describe 'panko::db::mysql' do
         required_params
       end
 
+      it { should contain_class('panko::deps') }
+
       it { is_expected.to contain_openstacklib__db__mysql('panko').with(
         :user     => 'panko',
         :password => 'pankopass',
